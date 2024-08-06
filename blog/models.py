1a7f22072566011aace947ac72a9f8cc03f79fa7
Timea Mali <derzsi_timea@yahoo.com>
2024-08-06 12:33:58 +0000

    Add recipe model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+)
+
+def validate_nonzero(value):
+    if value <= 0:
+        raise ValidationError(f'{value} must be greater than zero.')
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipe_owner')
+    prep_time = models.PositiveIntegerField(
+        validators=[validate_nonzero, MaxValueValidator(300)], default=15)
+    cooking_time = models.PositiveIntegerField(
+        validators=[validate_nonzero, MaxValueValidator(600)], default=15)
+    servings = models.PositiveIntegerField(
+        validators=[validate_nonzero, MaxValueValidator(50)], default=1)
+    ingredients = models.TextField(blank=False)
+    instructions = models.TextField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
