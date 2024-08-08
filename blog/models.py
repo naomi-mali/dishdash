ec00cf16c74187c819983ac3ee1a6d34a67ba8a3
Timea Mali <derzsi_timea@yahoo.com>
2024-08-08 10:08:33 +0000

    Add views, URLs, and basic HTML template

--- blog/models.py
+++ blog/models.py
@@ -39,6 +39,7 @@ class Recipe(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+    excerpt = models.TextField(max_length=500, blank=True)
 
     class Meta:
         ordering = ["-created_on"]
