4efaf4a8fe1476a6cff4a067e7673a4cd101e6fe
Timea Mali <derzsi_timea@yahoo.com>
2024-12-03 19:43:04 +0000

    Add style to my buttons and Fix URL routing and admin page access; improve blog URL specificity

--- dishdash/urls.py
+++ dishdash/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
-    path('admin/', admin.site.urls),
+    
 
 ]
 
